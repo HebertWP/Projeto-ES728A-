51a4ec74df08e5b84502b9c6940d28d4d3887818
Hebert Wandick <wandick.hebert98@gmail.com>
2021-03-07 19:56:39 -0300

    add pre information

--- gerarMatrizControbilidade.m
+++ gerarMatrizControbilidade.m
@@ -1,3 +1,8 @@
+%{
+    @brief Similar to ctrb function
+    @param A system control matriz
+    @param B input matriz
+%}
 function E=gerarMatrizControbilidade(A,B)
     for i=1:size(A)         %add one row per time
         E(:,i)=A^(i-1)*B;   %A^i*B
